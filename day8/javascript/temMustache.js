9e29fc2c86a03d323bd7138598c92b6801dde63d
oscar <fosegura@gmail.com>
2012-03-23 12:35:46 -0300

    day 8

---
+++ day8/javascript/temMustache.js
@@ -0,0 +1,18 @@
+ var html = "<div>\
+            {{#perfil}}\
+            <p>Nombre: {{name}}</p>\
+            <p>Edad: {{age}}</p>\
+            <p>Estado Civil: {{civilstate}}</p>\
+            <p>Ciudad: {{city}}</p>\
+            <p>Pais: {{company}}</p>\
+            <p>Carrera: {{carrer}}</p>\
+            <p>Universidad: {{university}}</p>\
+            <p>Telefono: {{phone}}</p>\
+            <p>Email: {{email}}</p><br>\
+            {{/perfil}}\
+          </div>";
+
+$(document).ready(function(){
+    var output = Mustache.render( html, json)
+    $("#mustache").html(output);
+}); 
